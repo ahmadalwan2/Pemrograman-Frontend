3629a130c6642b3d1405826e86918e9355ba4196
ahmadalwan <madajadeh2023@gmail.com>
2025-12-29 13:47:29 +0700

    Menambahkan setTimeout dalam file async.js

--- pertemuan1/async.js
+++ pertemuan1/async.js
@@ -1,15 +1,21 @@
 // === Simulasi Asyncronous yang sederhana didalam JavaScript === //
 
 function rebusAir() {
+    setTimeout(() => {
     console.log("Rebus air");
+    }, 3000);
 }
 
 function masakMie() {
+    setTimeout(() => {  
     console.log("Masak mie");
+    }, 2000);
 }
 
 function makanMie() {
+    setTimeout(() => {
     console.log("Makan mie");
+    }, 4000);
 }
 
 rebusAir();
