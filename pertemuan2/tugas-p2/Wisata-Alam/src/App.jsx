9b5085d35787c4d40dfd636581bd2ea722072ba6
ahmadalwan <madajadeh2023@gmail.com>
2026-01-02 14:48:04 +0700

    Menambahkan beberapa tag dile App.jsx

--- pertemuan2/tugas-p2/Wisata-Alam/src/App.jsx
+++ pertemuan2/tugas-p2/Wisata-Alam/src/App.jsx
@@ -2,10 +2,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Navbar from '../components/Navbar/Navbar'
+import Header from '../components/Header/Header'
+import About from '../components/About/About'
+import Footer from '../components/Footer/Footer'
 function App() {
   return (
     <>
       <Navbar NusaGo={"NusaGo"} home={"Home"} about={"about"} service={"service"}/>
+
+      <Header Hero={"Selamat Datang di Website Kami"}/>
+
+      <About about={ "NusaGo adalah sebuah website wisata alam yang menyediakan beberapa layanan terbaik sepanjang masa" }/>
+
+      <Footer footer={"Copyright &copy;2026 Developed by NusaGo ❤️"}/>
     </>
   )
 }
