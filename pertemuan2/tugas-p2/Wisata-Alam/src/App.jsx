d717a5ea486ff9b7a1e754694b986c20bfddbfaf
ahmadalwan <madajadeh2023@gmail.com>
2026-01-02 14:32:25 +0700

    Menambahkan tag Navbar

--- pertemuan2/tugas-p2/Wisata-Alam/src/App.jsx
+++ pertemuan2/tugas-p2/Wisata-Alam/src/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Navbar from '../components/Navbar/Navbar'
 function App() {
   return (
     <>
-
+      <Navbar NusaGo={"NusaGo"} home={"Home"} about={"about"} service={"service"}/>
     </>
   )
 }
