44bca40b41883188fdb5f790a88b7d43b660ffe3
ahmadalwan <madajadeh2023@gmail.com>
2026-01-02 10:20:00 +0700

    Menambahkan data difile Profile.jsx

---
+++ pertemuan2/petik-blog/src/Profile.jsx
@@ -0,0 +1,12 @@
+const Profile = () => {
+    return (
+        <div>
+            <h3>ABout Me</h3>
+            <ul>
+                <li>Nama : </li>
+                <li>Alamat : </li>
+                <li>Umur : </li>
+            </ul>
+        </div>
+    )
+}
