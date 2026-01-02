3496e35679d98f5ed35c3521316f90bc5d4302fb
ahmadalwan <madajadeh2023@gmail.com>
2026-01-02 11:29:04 +0700

    Mengubah isi kedua file jsx

--- pertemuan2/petik-blog/src/App.jsx
+++ pertemuan2/petik-blog/src/App.jsx
@@ -1,18 +1,26 @@
 import './App.css'
-import Customer from './Customer'
+import { Component, useState } from 'react'
+import reactLogo from '../src/assets/react.svg'
+import viteLogo from '../../petik-blog/public/vite.svg'
+import Header from './components/Header/Header.jsx'
+import Footer from './components/Footer/Footer.jsx'
+import Customer from './components/Customer/Customer.jsx'
 
 
 function App() {
   return (
     <>
+    {/* <Header nama="Selamat Datang" tentang="Asal Depok"/> */}
       <Customer tag="Our Customer"/>
       <Customer nama="Ucup" alamat={"Depok"} member={"Gold"} />
       <Customer nama="Raja" alamat={"Jakarta"} member={"Silver"} />
       <Customer nama="Aldi" alamat={"Bandung"} member={"Platinum"} />
       <Customer nama="Alex" alamat={"Depok"} member={"Non member"} />
+    <Footer nama="wan.studio"/>
     </>
 
   )
 }
+
 
 export default App
