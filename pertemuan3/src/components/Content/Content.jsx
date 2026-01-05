0ca2bc33165de232f01ed0c4ae44515cc4ffb872
ahmadalwan <madajadeh2023@gmail.com>
2026-01-05 13:46:08 +0700

    Membuat isi content

--- pertemuan3/src/components/Content/Content.jsx
+++ pertemuan3/src/components/Content/Content.jsx
@@ -1,18 +1,42 @@
+import "./Content.css"
+
 const Content = ({ nama, membership }) => {
+        const tableStyle = {
+            // border: "1px solid black",
+            // width: "300px",
+            // margin: "10px auto",
+            // color: "black"
+        };
+
+        const headerStyle = {
+            // textAlign: "center",
+            // fontWeight: "bold",
+        };
+
+        const cellStyle = {
+            // textAlign: "center",
+        };
+
+        const buttonStyle = {
+            // backgroundColor: "blue",
+            // color: "white",
+        }
+
+
         return (
             <div>
-                <table border={1}>
-                    <thead>
+                <table className="card">
+                    <thead className="card-head">
                         <tr>
-                            <td colSpan={2} style={{ textAlign: "center", fontWeight: "bold"}}>{nama}</td>
+                            <td className="user" colSpan={2}>{nama}</td>
                         </tr>
                     </thead>
-                    <tbody>
+                    <tbody className="card-body">
                         <tr>
-                            <td colSpan={2} style={{ textAlign: "center"}}>{membership}</td>
+                            <td className="member" colSpan={2}>{membership}</td>
                         </tr>
                         <tr>
-                            <td>
+                        <td>
                                 <button>Message</button>
                             </td>
                         <td>
