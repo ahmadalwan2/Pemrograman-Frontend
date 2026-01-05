775e6ff9debe58b2da3075d4a20bd67ddcdbd9d8
ahmadalwan <madajadeh2023@gmail.com>
2026-01-05 13:53:50 +0700

    Membuat props

--- pertemuan3/src/Card/Card.jsx
+++ pertemuan3/src/Card/Card.jsx
@@ -1,16 +1,16 @@
-const Card = () => {
+const Card = ({ title, price, list1, list2, list3, list4 }) => {
         return (
             <div className="card">
                 <div className="card-header">
-                    <h3></h3>
-                    <h1></h1>
+                    <h3>{title}</h3>
+                    <h1>{price}</h1>
                 </div>
                 <div className="card-body">
                     <ul>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
+                        <li>{list1}</li>
+                        <li>{list2}</li>
+                        <li>{list3}</li>
+                        <li>{list4}</li>
                     </ul>
                     <button>Purchase plan</button>
                 </div>
