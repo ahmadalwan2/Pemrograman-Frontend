84349414941e30cc4c97165aca246cc44f594642
ahmadalwan <madajadeh2023@gmail.com>
2026-01-05 13:51:39 +0700

    Membuat file React

---
+++ pertemuan3/src/Card/Card.jsx
@@ -0,0 +1,19 @@
+const Card = () => {
+        return (
+            <div className="card">
+                <div className="card-header">
+                    <h3></h3>
+                    <h1></h1>
+                </div>
+                <div className="card-body">
+                    <ul>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </ul>
+                    <button>Purchase plan</button>
+                </div>
+            </div>
+        )
+}
