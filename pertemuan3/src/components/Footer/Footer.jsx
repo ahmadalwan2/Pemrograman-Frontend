56d7cbbd79811a14a44a69fdbe0d75092740696d
ahmadalwan <madajadeh2023@gmail.com>
2026-01-05 08:51:18 +0700

    Membuat fungsi di Footer

---
+++ pertemuan3/src/components/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+    return (
+        <div>
+            <h3>Copyright &copy; 2026 Developed by Wan.studio</h3>
+            <span>Make with &#10084</span>
+        </div>
+    )
+}
+
+export default Footer;
