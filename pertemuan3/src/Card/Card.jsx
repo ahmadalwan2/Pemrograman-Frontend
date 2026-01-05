9c3d24f8e7a32a6820522f74c9a41a844679c308
ahmadalwan <madajadeh2023@gmail.com>
2026-01-05 15:16:41 +0700

    Memperbaiki kode

--- pertemuan3/src/Card/Card.jsx
+++ pertemuan3/src/Card/Card.jsx
@@ -1,20 +1,31 @@
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
 import "./Card.css"
 
-const Card = ({ title, price, list1, list2, list3, list4 }) => {
+const Card = ({ title = 0, price, list1, list2, list3, list4, isPopular = false, }) => {
         return (
-            <div className="card">
-                <div className="card-header">
-                    <h3>{title}</h3>
-                    <h1>{price}</h1>
+            <div className={`card ${isPopular ? "card-popular" : ""}`}>
+                <div className={`card-header ${isPopular ? "card-popular" : ""}`}>
+                    <h3>
+                        {title}{" "} 
+                        <span 
+                            className={`${isPopular ? "badge-popular" : ""}`}>
+                                {isPopular ? "Popular" : ""}
+                        </span>
+                    </h3>
+                    <h1>{price} 
+                        <small>/Month</small>
+                    </h1>
+
                 </div>
                 <div className="card-body">
                     <ul>
-                        <li>{list1}</li>
-                        <li>{list2}</li>
-                        <li>{list3}</li>
-                        <li>{list4}</li>
+                    <li>{list1}</li>
+                    <li>{list2}</li>
+                    <li>{list3}</li>
+                    <li>{list4}</li>
                     </ul>
-                    <button>Purchase plan</button>
+                    <button className={`btn ${isPopular ? "btn-popular" : ""}`}>Purchase plan</button>
                 </div>
             </div>
         )
