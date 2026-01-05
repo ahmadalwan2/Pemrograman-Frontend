10b06c398ebba2965767f1615c378ecfe520a3d6
ahmadalwan <madajadeh2023@gmail.com>
2026-01-05 09:02:54 +0700

    Memanggil fungsi Content yang sudah dibuat

--- pertemuan3/src/App.jsx
+++ pertemuan3/src/App.jsx
@@ -2,13 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './components/Navbar/Navbar'
+import Content from './components/Content/Content'
+import Footer from '../../pertemuan2/tugas-p2/Wisata-Alam/components/Footer/Footer'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-
+    <Navbar/>
+    <Content nama="Ucup" membership="Platinum"/>
+    <Content nama="Bambang" membership="Gold"/>
+    <Content nama="Budi" membership="Silver"/>
+    <Footer/>
     </>
   )
 }
