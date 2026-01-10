2cc6e2eddf809a0ec120b38bb4b2a782ae9a414c
ahmadalwan <madajadeh2023@gmail.com>
2026-01-10 17:38:13 +0700

    Membuat foolder Navbar

--- pertemuan3/tugas-p3/NusaGo/components/Navbar/Navbar.jsx
+++ pertemuan3/tugas-p3/NusaGo/components/Navbar/Navbar.jsx
@@ -3,12 +3,12 @@ import "./Navbar.css";
 function Navbar({}) {
     return (
         <nav className="navbar">
-            <span className="logo-navbar">NusaGo</span>
+            <span className="logo-navbar">Ahgoda</span>
             <ul>
                 <li><a href="#">Home</a></li>
-                <li><a href="#">About</a></li>
-                <li><a href="#">Booking</a></li>
-                <li><a href="#">Services</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#services">Services</a></li>
+                <li><a href="#footer">FAQ</a></li>
             </ul>
         </nav>
     )
