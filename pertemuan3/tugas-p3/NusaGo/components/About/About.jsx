0d9cbb819ed30a8d33acf244a1ab655f41f86d32
ahmadalwan <madajadeh2023@gmail.com>
2026-01-10 17:37:09 +0700

    Membuat folder About

---
+++ pertemuan3/tugas-p3/NusaGo/components/About/About.jsx
@@ -0,0 +1,39 @@
+import './About.css';
+
+function About({ txt }) {
+  return (
+    <section className="about" id="about">
+      <div className="about-container">
+        <h2 className="about-title">{txt}</h2>
+        <p className="about-text">
+          NusaGO hadir untuk membantu Anda menjelajahi keindahan alam Indonesia. 
+          <br />Kami menyediakan informasi destinasi wisata, hotel, kuliner, dan penginapan 
+          yang sempurna untuk petualangan Anda.
+        </p>
+
+        <div className="about-imgs">
+          <img src="/img/04.jpg" alt="Destinasi 1" />
+          <img src="/img/05.jpg" alt="Destinasi 2" />
+          <img src="/img/06.jpg" alt="Destinasi 3" />
+        </div>
+
+        <div className="about-stats">
+          <div className="stat-box">
+            <h3 className="stat-number">120,000+</h3>
+            <p className="stat-label">Total Pengunjung</p>
+          </div>
+          <div className="stat-box">
+            <h3 className="stat-number">325</h3>
+            <p className="stat-label">Total Destinasi</p>
+          </div>
+          <div className="stat-box">
+            <h3 className="stat-number">4.9 <span className='starYellow'>★</span></h3>
+            <p className="stat-label">Rating Pengguna</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default About;
