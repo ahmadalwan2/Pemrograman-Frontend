f008c52c0d313a38b00e5d693b64456e3a384042
ahmadalwan <madajadeh2023@gmail.com>
2026-01-10 17:37:28 +0700

    Membuat folder Footer

--- pertemuan3/tugas-p3/NusaGo/components/Footer/Footer.jsx
+++ pertemuan3/tugas-p3/NusaGo/components/Footer/Footer.jsx
@@ -1,2 +1,32 @@
 import './Footer.css';
 
+import { FaFacebookF, FaInstagram, FaTwitter } from "react-icons/fa";
+
+function Footer() {
+  return (
+    <footer className="footer" id='footer'>
+      <div className="footer-content">
+        <h2 className="footer-logo">Ahgoda</h2>
+        <p className="footer-desc">Semua kebutuhan wisata Anda dalam satu tempat</p>
+
+        <div className="footer-links">
+          <a href="#about">Tentang Kami</a>
+          <a href="#services">Layanan</a>
+          <a href="#">FAQ</a>
+        </div>
+
+        <div className="footer-social">
+          <a href="#"><FaFacebookF /></a>
+          <a href="#"><FaInstagram /></a>
+          <a href="#"><FaTwitter /></a>
+        </div>
+      </div>
+
+      <div className="footer-bottom">
+        <p>© 2026 Ahgoda. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
