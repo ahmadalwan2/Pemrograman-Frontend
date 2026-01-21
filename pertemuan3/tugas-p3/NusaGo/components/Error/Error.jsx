1ef89bff354a890cf9a5575b5cedb07d475fac27
ahmadalwan <madajadeh2023@gmail.com>
2026-01-21 21:36:08 +0700

    Mengubah isi folder Error

--- pertemuan3/tugas-p3/NusaGo/components/Error/Error.jsx
+++ pertemuan3/tugas-p3/NusaGo/components/Error/Error.jsx
@@ -1,14 +1,19 @@
 import './Error.css';
 import Navbar from '../Navbar/Navbar.jsx';
 import Footer from '../Footer/Footer.jsx';
+import { useNavigate } from 'react-router-dom';
 
 function ErrorPage() {
+        const navigate = useNavigate();
     return(
         <div>
             <Navbar/>
             <div className='container'>
                 <h1 className='title'>404</h1>
-                <p className='text'>Not Found</p>
+                <p className='text'>Halaman tidak tersedia</p>
+                <div className='btn'>
+                <button onClick={() => navigate (-1)}>Kembali</button>
+                </div>
             </div>
             <Footer/>
         </div>
