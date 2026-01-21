aa13db1e4a74147857d3fe72348c810541fba694
ahmadalwan <madajadeh2023@gmail.com>
2026-01-21 14:32:10 +0700

    Mengubah isi file Header.jsx

--- pertemuan3/tugas-p3/NusaGo/components/Header/Header.jsx
+++ pertemuan3/tugas-p3/NusaGo/components/Header/Header.jsx
@@ -1,7 +1,12 @@
 import './Header.css';
+import Navbar from '../Navbar/Navbar.jsx';
+import { Link } from 'react-router-dom';
+import Footer from '../Footer/Footer.jsx';
+
 function Header() {
     return (
         <div className='Home' id='home'>
+            <Navbar/>
             <div className='photo-row'>
                 <img src="../../public/img/01.jpg" alt="this photo not support in your device" />
                 <img src="../../public/img/02.jpg" alt="this photo not support in your device" />
@@ -10,8 +15,9 @@ function Header() {
             <h2 className='Hero'>Jelajahi Keindahan <br />Alam Indonesia</h2>
             <p className='CTA'>Temukan destinasi wisata alam terbaik, hotel, kuliner, dan <br />penginapan yang sempurna untuk petualangan Anda</p>
             <div className='divBtn'>
-            <button className='btnHeader'><a href="#search">Jelajahi Sekarang</a></button>
+            <button className='btnHeader'><Link to={'/search'}>Jelajahi Sekarang</Link></button>
             </div>
+            <Footer/>
         </div>
     )
 }
